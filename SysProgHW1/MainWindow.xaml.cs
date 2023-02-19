0cb93999ae66488b330f95a47d43d403ce3ecaec
Leyla Shafiyeva <llshafi03@gmail.com>
2023-02-19 17:37:44 +0400

    Add project files.

---
+++ SysProgHW1/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SysProgHW1;
+
+public partial class MainWindow : Window
+{
+    public ObservableCollection<Process> Processes { get; set; }
+    public List<string> BlackList { get; set; }
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = this;
+        Processes = new(Process.GetProcesses());
+        BlackList = new();
+
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(SearchTxt.Text))
+        {
+            MessageBox.Show("Enter process name");
+            return;
+        }
+
+        try
+        {
+            ProcessStartInfo startInfo = new(SearchTxt.Text);
+
+            startInfo.WindowStyle = ProcessWindowStyle.Minimized;
+
+            Process.Start(startInfo);
+            SearchTxt.Text = String.Empty;
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+        finally
+        {
+            Dispatcher.Invoke(() =>
+            {
+                Processes.Clear();
+                foreach (var p in Process.GetProcesses())
+                    Processes.Add(p);
+            }
+            );
+
+        }
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+        if (List.SelectedItem is null)
+            return;
+
+        if (List.SelectedItem is Process process)
+        {
+            try
+            {
+                process.Kill();
+                Processes.Remove(process);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    Processes.Clear();
+                    foreach (var p in Process.GetProcesses())
+                        Processes.Add(p);
+                }
+                );
+        
+            }
+
+        }
+    }
+}
